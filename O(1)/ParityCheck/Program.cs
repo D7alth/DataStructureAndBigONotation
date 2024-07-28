b801ba34d4803d085644b4fe1445bb49785f3e00
alberth henrique <alberthhls@protonmail.com>
2024-07-28 16:43:34 -0300

    ✨ Added new O(1) algorithm "ParityCheck"

---
+++ O(1)/ParityCheck/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter with a value: ");
+var input = Int32.Parse(Console.ReadLine());
+if (input < 0)
+{
+    Console.WriteLine("The valor not be a less to 0");
+    return;
+}
+var isEven = IsEven(input);
+Console.WriteLine(isEven ? "The number is Even" : "The number is odd");
+
+static bool IsEven(int n) => n % 2 == 0;
