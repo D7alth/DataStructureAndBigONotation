30620dbfcb396da47702d2316420ac173c27c128
alberth henrique <alberthhls@protonmail.com>
2024-07-28 16:34:49 -0300

    ✨ Added new O(1) algorithm "HashTableInsert"

---
+++ O(1)/HashTableInsert/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+Console.WriteLine("Insert a element: ");
+var input = Console.ReadLine();
+
+var hashTable = new Hashtable();
+hashTable.Add(1, input);
+
+Console.WriteLine($"Value inserted: {hashTable[1]}");
+
+hashTable.Add(2, "test 01");
+hashTable.Add(3, "test 02");
+
+Console.WriteLine("Add new value: ");
+input = Console.ReadLine();
+hashTable.Add(4, input);
+
+Console.WriteLine("Insert A key: ");
+var key = Int32.Parse(Console.ReadLine());
+
+if (hashTable.ContainsKey(key))
+    Console.WriteLine($"The value is: {hashTable[key]}");
+else
+    Console.WriteLine($"This key don't exists");
